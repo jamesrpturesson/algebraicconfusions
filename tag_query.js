61aed3bd897ca81ee83857c9fa97782516ac514a
JamesTuresson <52086402+jamesrpturesson@users.noreply.github.com>
2024-06-16 11:10:23 +0200

    Update tag_query.js

--- tag_query.js
+++ tag_query.js
@@ -1,10 +1,12 @@
 let query = location.search.split('?')[1];
 let params = query.split('&');
 let par = null;
+let tags = [];
 for (i=0; i < params.length; i++) {
     console.log(params[i]);
     par = params[i].split('=');
     if (par[0] == 'tags'){
-        console.log('yes');
+        tags = par[1].split(',');
     }
 }
+console.log(tags);
